1868a21a8e5d509819bc2e149f5324be1d809fbb
Elenadame <54961621+Elenadame@users.noreply.github.com>
2020-01-24 14:43:15 -0800

    spelling errors fixed

--- quotes.js
+++ quotes.js
@@ -1,11 +1,11 @@
 var quotes=[
-'Do or do not, there is no try. - Joda',
-'Don\'t let anyone dull your sparkle. -?',
+'Do. Or do not. There is no try. - Joda',
 'To infinity and beyond. - Toy Story',
-'Be yourself; everyone else if already taken - Oscar Wilde',
+'Be yourself; everyone else is already taken - Oscar Wilde',
 'I never look back, darling! It distracts me from the now. - Edna Mode',
 'It does not dwell on the dream and forget to live. - Albus Dumbledore',
-'Do one thing every day that scare you. - Eleanor Rososevelt'
+'Do one thing every day that scares you. - Eleanor Rososevelt'
+'While it is great to believe in one self, a little help from others can be a great blessing. -Iroh'
 ]
 
 function randomQuote(){
