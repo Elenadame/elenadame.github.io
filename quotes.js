23489d5dd622fbc297b5b0afec3e9844d5424b8c
Elenadame <54961621+Elenadame@users.noreply.github.com>
2020-01-24 14:56:22 -0800

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -4,7 +4,7 @@ var quotes=[
 'Be yourself; everyone else is already taken - Oscar Wilde',
 'I never look back, darling! It distracts me from the now. - Edna Mode',
 'It does not dwell on the dream and forget to live. - Albus Dumbledore',
-'Do one thing every day that scares you. - Eleanor Rososevelt'
+'Do one thing every day that scares you. - Eleanor Rososevelt',
 'While it is great to believe in one self, a little help from others can be a great blessing. -Iroh'
 ]
 
